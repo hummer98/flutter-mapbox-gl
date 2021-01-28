984f49948f4bc43454fcbce56455eb1ea65ad9dc
Yuji Yamamoto <rr.yamamoto@gmail.com>
2021-01-28 15:30:28 +0900

    change reference at path

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -2,15 +2,12 @@
 // Generated file. Do not edit.
 //
 
-// ignore: unused_import
-import 'dart:ui';
-
 import 'package:mapbox_gl_web/mapbox_gl_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(PluginRegistry registry) {
-  MapboxMapPlugin.registerWith(registry.registrarFor(MapboxMapPlugin));
-  registry.registerMessageHandler();
+void registerPlugins(Registrar registrar) {
+  MapboxMapPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
 }
